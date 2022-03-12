56686109087e9e934c78fe09b84d6397442f7298
chandu <chandrachandu0713@gmail.com>
2022-03-12 16:21:50 +0530

    Session-1

---
+++ src/main/java/com/everest/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.everest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out= resp.getWriter();
+		
+		String username= req.getParameter("chandu");
+		String password= req.getParameter("don");
+		
+		out.print("Welcome "+username);
+		out.print("Your password is "+password);
+	}
+}
